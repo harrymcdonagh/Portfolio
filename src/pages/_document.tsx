8dec5438e9a2032aedab1fffd7c4552e1fc266a3
Harry McDonagh <harrymcdonagh82@gmail.com>
2024-08-04 17:49:57 +0100

    Added page title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <head>
+        <title>Harry McDonagh</title>
+      </head>
       <Head />
       <body id="root" className="bg-zinc-900 text-zinc-50">
         <Main />
