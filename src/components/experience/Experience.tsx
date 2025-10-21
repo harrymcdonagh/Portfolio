4dbc63f99d725429d963ac685a1a673eceed873a
Harry McDonagh <harrymcdonagh82@gmail.com>
2025-10-21 16:35:44 +0100

    Updated CV, add Puritan AI exp

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -14,6 +14,27 @@ export const Experience = () => {
 
 const experience = [
   {
+    title: "Puritan AI",
+    position: "AI Associate Intern",
+    time: "June 2024 - September 2024",
+    location: "London",
+    description: [
+      "Contributed to applied research on AI-driven automation, analysing data workflows and identifying opportunities to improve operational efficiency",
+      "Assisted in prompt engineering and model evaluation, tuning responses for accuracy, relevance and reduced hallucinations.",
+      "Supported the prototyping of automated client-support systems, improving response times and documentation processes across multiple business use cases",
+    ],
+    tech: [],
+  },
+  {
+    title: "JP Morgan",
+    position: "Software Engineering Virtual Experience on Forage",
+    time: "February 2024",
+    location: "Online ",
+    description:
+      "Established and maintained a local development environment, rectified broken files to optimise web application performance, and used JPMorgan Chase's Perspective library to create real-time, visually appealing data visualisations for trader monitoring.",
+    tech: ["Git", "Python", "Perspective"],
+  },
+  {
     title: "Horizone Ltd",
     position: "IT Technician",
     time: "February 2022 - September 2022",
@@ -25,14 +46,5 @@ const experience = [
       "Enhanced office connectivity and system performance by building and troubleshooting network infrastructures",
     ],
     tech: ["VBA", "Office 365", "SQL"],
-  },
-  {
-    title: "JP Morgan",
-    position: "Software Engineering Virtual Experience on Forage",
-    time: "February 2024",
-    location: "Online ",
-    description:
-      "Established and maintained a local development environment, rectified broken files to optimise web application performance, and used JPMorgan Chase's Perspective library to create real-time, visually appealing data visualisations for trader monitoring.",
-    tech: ["Git", "Python", "Perspective"],
   },
 ];
